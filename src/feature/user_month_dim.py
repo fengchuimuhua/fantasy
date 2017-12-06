256a4f03be4d87104f480d3cfb45541838128df4
丁雪涛 <dingxuetao@meituan.com>
2017-12-07 00:35:54 +0800

    fix

--- src/feature/user_month_dim.py
+++ src/feature/user_month_dim.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import sys
 from sklearn import linear_model
 from sklearn.metrics import r2_score
