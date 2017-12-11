bdf6dc1bf5987c0c0fb0b688d84596afbcdeea53
丁雪涛 <dingxuetao@meituan.com>
2017-12-11 22:56:51 +0800

    add fea

--- src/model/1210_valid.py
+++ src/model/1210_valid.py
@@ -26,18 +26,18 @@ fea_user_fn = '../../fea/fea_user.csv'
 fea_user_date_fn = '../../fea/fea_user_date.csv'
 fea_limit_date_fn = '../../fea/fea_limit_date.csv'
 fea_user_month_fn = '../../fea/fea_user_month.csv'
-fea_age_date_fn = '../../fea/fea_age_date.csv'
-fea_sex_date_fn = '../../fea/fea_sex_date.csv'
-fea_age_sex_date_fn = '../../fea/fea_age_sex_date.csv'
+#fea_age_date_fn = '../../fea/fea_age_date.csv'
+#fea_sex_date_fn = '../../fea/fea_sex_date.csv'
+#fea_age_sex_date_fn = '../../fea/fea_age_sex_date.csv'
 label_user_date_fn = '../../label/user_date_label.csv'
 
 user_df = pd.read_csv(fea_user_fn)
 user_date_df = pd.read_csv(fea_user_date_fn)
 limit_date_df = pd.read_csv(fea_limit_date_fn)
 user_month_df = pd.read_csv(fea_user_month_fn)
-age_date_df = pd.read_csv(fea_age_date_fn)
-sex_date_df = pd.read_csv(fea_sex_date_fn)
-age_sex_date_df = pd.read_csv(fea_age_sex_date_fn)
+#age_date_df = pd.read_csv(fea_age_date_fn)
+#sex_date_df = pd.read_csv(fea_sex_date_fn)
+#age_sex_date_df = pd.read_csv(fea_age_sex_date_fn)
 user_date_label_df = pd.read_csv(label_user_date_fn)
 
 print 'step 1. read done'
@@ -45,16 +45,16 @@ print 'step 1. read done'
 user_date_df = user_dim.merge(user_df, user_date_df)
 user_date_df = limit_date_dim.merge(limit_date_df, user_date_df)
 user_date_df = user_month_dim.merge(user_month_df, user_date_df)
-user_date_df = age_date_dim.merge(age_date_df, user_date_df)
-user_date_df = sex_date_dim.merge(sex_date_df, user_date_df)
-user_date_df = age_sex_date_dim.merge(age_sex_date_df, user_date_df)
+#user_date_df = age_date_dim.merge(age_date_df, user_date_df)
+#user_date_df = sex_date_dim.merge(sex_date_df, user_date_df)
+#user_date_df = age_sex_date_dim.merge(age_sex_date_df, user_date_df)
 user_date_df = user_date_label.merge(user_date_label_df, user_date_df)
 
 print 'step 2. join data done'
 
 #feature_col_list = ['u_age', 'u_sex', 'u_limit', 'ud_active_days', 'ud_clk_cnt_1d', 'ud_clk_cnt_3d', 'ud_clk_cnt_7d', 'ud_clk_cnt_14d', 'ud_clk_cnt_21d', 'ud_clk_cnt_30d', 'ud_clk_cnt_60d','ud_clk_cnt_90d', 'ud_ord_cnt_1d', 'ud_ord_cnt_3d', 'ud_ord_cnt_7d', 'ud_ord_cnt_14d', 'ud_ord_cnt_21d', 'ud_ord_cnt_30d', 'ud_ord_cnt_60d', 'ud_ord_cnt_90d', 'ud_ord_price_21d', 'ud_ord_price_30d', 'ud_ord_price_60d', 'ud_ord_pay_price_21d', 'ud_ord_pay_price_30d', 'ud_ord_pay_price_60d', 'ud_ord_pay_price_ratio_21d', 'ud_ord_pay_price_ratio_30d', 'ud_ord_pay_price_ratio_60d', 'ud_ctr_1d', 'ud_ctr_3d', 'ud_ctr_7d', 'ud_ctr_14d', 'ud_ctr_21d', 'ud_ctr_30d', 'ud_ctr_60d', 'ud_ctr_90d', 'ud_loan_1d', 'ud_loan_3d', 'ud_loan_7d', 'ud_loan_14d', 'ud_loan_21d', 'ud_loan_30d', 'ud_loan_60d','ud_loan_90d', 'ud_loan_num_30d', 'ud_loan_num_60d', 'ud_loan_num_90d', 'ud_loan_max_7d', 'ud_loan_max_14d', 'ud_loan_max_21d', 'ud_loan_max_30d', 'ud_loan_max_60d', 'ud_loan_max_90d', 'ud_loan_min_7d', 'ud_loan_min_14d', 'ud_loan_min_21d', 'ud_loan_min_30d', 'ud_loan_min_60d', 'ud_loan_min_90d', 'ud_loan_std_7d', 'ud_loan_std_14d', 'ud_loan_std_21d', 'ud_loan_std_30d', 'ud_loan_std_60d', 'ud_loan_std_90d', 'ud_loan_mean_7d', 'ud_loan_mean_14d', 'ud_loan_mean_21d', 'ud_loan_mean_30d', 'ud_loan_mean_60d', 'ud_loan_mean_90d', 'ud_loan_skew_7d', 'ud_loan_skew_14d', 'ud_loan_skew_21d', 'ud_loan_skew_30d', 'ud_loan_skew_60d', 'ud_loan_skew_90d', 'ud_loan_mad_7d', 'ud_loan_mad_14d', 'ud_loan_mad_21d', 'ud_loan_mad_30d', 'ud_loan_mad_60d', 'ud_loan_mad_90d', 'ud_real_loan_30d','ud_real_loan_60d','ud_real_loan_90d', 'ud_real_loan_max_30d','ud_real_loan_max_60d','ud_real_loan_max_90d', 'ud_real_loan_min_30d','ud_real_loan_min_60d','ud_real_loan_min_90d', 'ud_real_loan_kurt_30d','ud_real_loan_kurt_60d','ud_real_loan_kurt_90d', 'ud_real_loan_skew_30d','ud_real_loan_skew_60d','ud_real_loan_skew_90d', 'ud_real_loan_std_30d','ud_real_loan_std_60d','ud_real_loan_std_90d', 'ud_plannum_21d','ud_plannum_30d','ud_plannum_60d', 'ud_plannum_max_21d', 'ud_plannum_max_30d', 'ud_plannum_max_60d','ud_plannum_max_90d', 'ud_plannum_min_21d', 'ud_plannum_min_30d', 'ud_plannum_min_60d','ud_plannum_min_90d', 'ud_plannum_kurt_21d','ud_plannum_kurt_30d','ud_plannum_kurt_60d','ud_plannum_kurt_90d', 'ud_plannum_skew_21d','ud_plannum_skew_30d','ud_plannum_skew_60d','ud_plannum_skew_90d','ud_plannum_std_21d', 'ud_plannum_std_30d', 'ud_plannum_std_60d','ud_plannum_std_90d', 'ld_limit_cat', 'ld_loan_1d', 'ld_loan_3d', 'ld_loan_7d', 'ld_loan_14d', 'ld_loan_21d', 'ld_loan_30d', 'ld_loan_60d', 'ld_loan_90d', 'um_left_balance','um_month_need_pay','um_tuned_limit']
 
-feature_col_list = ['u_age', 'u_sex', 'u_limit', 'ud_active_days', 'ud_clk_cnt_1d', 'ud_clk_cnt_3d', 'ud_clk_cnt_7d', 'ud_clk_cnt_14d', 'ud_clk_cnt_21d', 'ud_clk_cnt_30d', 'ud_clk_cnt_60d','ud_clk_cnt_90d', 'ud_ord_cnt_1d', 'ud_ord_cnt_3d', 'ud_ord_cnt_7d', 'ud_ord_cnt_14d', 'ud_ord_cnt_21d', 'ud_ord_cnt_30d', 'ud_ord_cnt_60d', 'ud_ord_cnt_90d', 'ud_ord_price_21d', 'ud_ord_price_30d', 'ud_ord_price_60d', 'ud_ord_pay_price_21d', 'ud_ord_pay_price_30d', 'ud_ord_pay_price_60d', 'ud_ord_pay_price_ratio_21d', 'ud_ord_pay_price_ratio_30d', 'ud_ord_pay_price_ratio_60d', 'ud_ctr_1d', 'ud_ctr_3d', 'ud_ctr_7d', 'ud_ctr_14d', 'ud_ctr_21d', 'ud_ctr_30d', 'ud_ctr_60d', 'ud_ctr_90d', 'ud_loan_1d', 'ud_loan_3d', 'ud_loan_7d', 'ud_loan_14d', 'ud_loan_21d', 'ud_loan_30d', 'ud_loan_60d','ud_loan_90d', 'ud_loan_num_30d', 'ud_loan_num_60d', 'ud_loan_num_90d', 'ud_loan_max_7d', 'ud_loan_max_14d', 'ud_loan_max_21d', 'ud_loan_max_30d', 'ud_loan_max_60d', 'ud_loan_max_90d', 'ud_loan_min_7d', 'ud_loan_min_14d', 'ud_loan_min_21d', 'ud_loan_min_30d', 'ud_loan_min_60d', 'ud_loan_min_90d', 'ld_limit_cat', 'ld_loan_1d', 'ld_loan_3d', 'ld_loan_7d', 'ld_loan_14d', 'ld_loan_21d', 'ld_loan_30d', 'ld_loan_60d', 'ld_loan_90d', 'um_left_balance','um_month_need_pay','um_tuned_limit', 'ud_loan_std_7d', 'ud_loan_std_14d', 'ud_loan_std_21d', 'ud_loan_std_30d', 'ud_loan_std_60d', 'ud_loan_std_90d', 'ud_loan_mean_7d', 'ud_loan_mean_14d', 'ud_loan_mean_21d', 'ud_loan_mean_30d', 'ud_loan_mean_60d', 'ud_loan_mean_90d', 'ud_loan_skew_7d', 'ud_loan_skew_14d', 'ud_loan_skew_21d', 'ud_loan_skew_30d', 'ud_loan_skew_60d', 'ud_loan_skew_90d', 'ud_loan_mad_7d', 'ud_loan_mad_14d', 'ud_loan_mad_21d', 'ud_loan_mad_30d', 'ud_loan_mad_60d', 'ud_loan_mad_90d', 'ud_real_loan_30d','ud_real_loan_60d','ud_real_loan_90d', 'ud_real_loan_max_30d','ud_real_loan_max_60d','ud_real_loan_max_90d', 'ud_real_loan_min_30d','ud_real_loan_min_60d','ud_real_loan_min_90d', 'ud_real_loan_kurt_30d','ud_real_loan_kurt_60d','ud_real_loan_kurt_90d', 'ud_real_loan_skew_30d','ud_real_loan_skew_60d','ud_real_loan_skew_90d', 'ud_real_loan_std_30d','ud_real_loan_std_60d','ud_real_loan_std_90d']
+feature_col_list = ['u_age', 'u_sex', 'u_limit', 'ud_active_days', 'ud_clk_cnt_1d', 'ud_clk_cnt_3d', 'ud_clk_cnt_7d', 'ud_clk_cnt_14d', 'ud_clk_cnt_21d', 'ud_clk_cnt_30d', 'ud_clk_cnt_60d','ud_clk_cnt_90d', 'ud_ord_cnt_1d', 'ud_ord_cnt_3d', 'ud_ord_cnt_7d', 'ud_ord_cnt_14d', 'ud_ord_cnt_21d', 'ud_ord_cnt_30d', 'ud_ord_cnt_60d', 'ud_ord_cnt_90d', 'ud_ord_price_21d', 'ud_ord_price_30d', 'ud_ord_price_60d', 'ud_ord_pay_price_21d', 'ud_ord_pay_price_30d', 'ud_ord_pay_price_60d', 'ud_ord_pay_price_ratio_21d', 'ud_ord_pay_price_ratio_30d', 'ud_ord_pay_price_ratio_60d', 'ud_ctr_1d', 'ud_ctr_3d', 'ud_ctr_7d', 'ud_ctr_14d', 'ud_ctr_21d', 'ud_ctr_30d', 'ud_ctr_60d', 'ud_ctr_90d', 'ud_loan_1d', 'ud_loan_3d', 'ud_loan_7d', 'ud_loan_14d', 'ud_loan_21d', 'ud_loan_30d', 'ud_loan_60d','ud_loan_90d', 'ud_loan_num_30d', 'ud_loan_num_60d', 'ud_loan_num_90d', 'ud_loan_max_7d', 'ud_loan_max_14d', 'ud_loan_max_21d', 'ud_loan_max_30d', 'ud_loan_max_60d', 'ud_loan_max_90d', 'ud_loan_min_7d', 'ud_loan_min_14d', 'ud_loan_min_21d', 'ud_loan_min_30d', 'ud_loan_min_60d', 'ud_loan_min_90d', 'ld_limit_cat', 'ld_loan_1d', 'ld_loan_3d', 'ld_loan_7d', 'ld_loan_14d', 'ld_loan_21d', 'ld_loan_30d', 'ld_loan_60d', 'ld_loan_90d', 'um_left_balance','um_month_need_pay','um_tuned_limit', 'ud_loan_std_7d', 'ud_loan_std_14d', 'ud_loan_std_21d', 'ud_loan_std_30d', 'ud_loan_std_60d', 'ud_loan_std_90d', 'ud_loan_mean_7d', 'ud_loan_mean_14d', 'ud_loan_mean_21d', 'ud_loan_mean_30d', 'ud_loan_mean_60d', 'ud_loan_mean_90d', 'ud_loan_skew_7d', 'ud_loan_skew_14d', 'ud_loan_skew_21d', 'ud_loan_skew_30d', 'ud_loan_skew_60d', 'ud_loan_skew_90d', 'ud_loan_mad_7d', 'ud_loan_mad_14d', 'ud_loan_mad_21d', 'ud_loan_mad_30d', 'ud_loan_mad_60d', 'ud_loan_mad_90d', 'ud_real_loan_30d','ud_real_loan_60d','ud_real_loan_90d', 'ud_real_loan_max_30d','ud_real_loan_max_60d','ud_real_loan_max_90d']
 
 # label: is_loan_in_31d, loan_amount_in_31d
 
